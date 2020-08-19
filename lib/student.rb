3db17347730e9ab8d42d91464ab08179bee8ca4c
Ryna Truong <rynatruong@outlook.com>
2020-08-19 16:25:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,4 +26,12 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def save
+    sql = <<-SQL 
+    INSERT INTO students(name, grade)
+    VALUES(?,?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
 end
