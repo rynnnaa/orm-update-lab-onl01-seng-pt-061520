768c53a71cd943cf1eab400c1f9ca26e23b0aef4
Ryna Truong <rynatruong@outlook.com>
2020-08-19 16:20:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS STUDENTS(
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
     name TEXT,
     grade TEXT
     )
