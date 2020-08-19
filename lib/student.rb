be47f52fbd3c960e4a2121cad7c35384be75dc0e
Ryna Truong <rynatruong@outlook.com>
2020-08-19 16:21:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,8 @@
 require_relative "../config/environment.rb"
 
 class Student
-  
-  attr_reader :id 
-  attr_accessor :name, :grade
+
+  attr_accessor :id, :name, :grade
   
   def initialize(id = nil, name, grade)
     @id = id
