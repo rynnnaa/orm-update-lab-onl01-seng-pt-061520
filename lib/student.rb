3bc2a0b041dc708b05540af334591c402f2272c6
Ryna Truong <rynatruong@outlook.com>
2020-08-19 16:15:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,13 @@
 require_relative "../config/environment.rb"
 
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
-
+  
+  attr_reader :id 
+  attr_accessor :name, :grade
+  
+  def initialize(id, name, grade)
+    @id = nil
+    @name = name
+    @grade = grade
+  end
 end
