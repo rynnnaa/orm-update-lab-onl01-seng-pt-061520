7c68ddf507c8eccf9b544b945bb973d79231222b
Ryna Truong <rynatruong@outlook.com>
2020-08-19 16:25:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,6 @@ class Student
     INSERT INTO students(name, grade)
     VALUES(?,?)
     SQL
-    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute(sql, self.name, self.grade)
   end
 end
