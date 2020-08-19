c450a19cda7b75ee2059644f3ee12fa3c40a083a
Ryna Truong <rynatruong@outlook.com>
2020-08-19 16:38:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,5 +41,7 @@ class Student
   end
   
   def update
-    sql = "UPDATE "
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
